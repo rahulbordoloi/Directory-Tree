3140e986bc9a8a695f5f3c34476ea54d2db5a6db
rahulbordoloi <rahulbordoloi24@gmail.com>
2020-10-06 12:46:17 +0530

    Directory Tree [v0.0.1]

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+Change Log
+==========
+
+1.0.0 (06/10/2020)
+-------------------
+- First Release (Initial)
+*  Prettified Tree Structure
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+
+    name = 'directory_tree',
+    version = '0.0.1',
+    description = 'Utility Package that displays out the Tree Structure of a Particular Directory.',
+    url = "https://github.com/rahulbordoloi/Directory-Tree/",
+    author = "Rahul Bordoloi",
+    author_email = "rahulbordoloi24@gmail.com",
+
+    py_modules = ['directory_tree'],
+    package_dir = {'': 'src'},
+
+    classifiers = [
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Utilities",
+        "Natural Language :: English",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Customer Service",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop"
+    ],
+
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+
+    extras_require = {
+        "dev" : [
+            "pytest >= 3.7",
+        ],
+    },
+)
